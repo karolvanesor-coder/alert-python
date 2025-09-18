8c292b0c0804f63543b280254597fe63e2d53459
karolvanesor-coder <karolvanesor@gmail.com>
2025-09-18 11:09:02 -0500

    update coment

--- test_post_mac.py
+++ test_post_mac.py
@@ -1,4 +1,3 @@
-//esta test es para mac
 curl -X POST http://127.0.0.1:5006/datadog-webhook \
 -H "Content-Type: application/json" \
 -d '{"title":"Prueba Explosión","body":"Se activó la alarma con imagen"}'
